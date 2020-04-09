5b2393c434b22b62838b8b2e8d517ceaafc51d58
Zoey Chesny <z.chesny@gmail.com>
2020-04-09 16:29:52 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/\b[aeiouAEIOU]\w*/) == nil
-    return false
-  else
-    return true
-  end
+  if word.match(/\b[aeiouAEIOU]\w*/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)

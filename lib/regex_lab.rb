bd0a27b63846a1381d025abec3cbc2173f6aded6
Zoey Chesny <z.chesny@gmail.com>
2020-04-09 16:21:03 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\A[A-Z]\z[.!?]/)
+  text.match(/\A[A-Z].*[.!?]/)
 end
 
 def valid_phone_number?(phone)

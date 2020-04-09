6b00e810ac4bb9853ca86fdc9ecf358f9cc8886d
Zoey Chesny <z.chesny@gmail.com>
2020-04-09 16:19:28 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,21 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiouAEIOU]\w*/) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/\A[A-Z]\z[.!?]/)
 end
 
 def valid_phone_number?(phone)

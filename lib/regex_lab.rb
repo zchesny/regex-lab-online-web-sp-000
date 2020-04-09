33371c0413bdea7e575c4e9381fed804e6e24873
Zoey Chesny <z.chesny@gmail.com>
2020-04-09 16:35:14 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  text.match(/\W?[0-9])\W?{3}[0-9][-]?{3}[0-9]{4}/) ? true : false
+  text.match(/\W?[0-9]\W?{3}[0-9][-]?{3}[0-9]{4}/) ? true : false
 end
